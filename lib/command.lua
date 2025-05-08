5d65418fdea02718f311d41604f21190f587d2ec
thederpykrafter <thederpykrafter@gmail.com>
2025-05-07 20:02:16 -0400

    param comments

--- lib/command.lua
+++ lib/command.lua
@@ -1,5 +1,7 @@
-
 -- Execute shell command and capture output
+---@param cmd string shell command
+---@param raw? boolean true to disable formatting
+---@return string, integer
 function os.capture(cmd, raw)
 	local f = assert(io.popen(cmd, "r"))
 	local s = assert(f:read("*a"))
